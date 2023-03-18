d920eef2c927834a14402e70c62bb55aeb60d214
Владимир <vovik190798@gmail.com>
2023-03-18 18:52:42 +0300

    sss

--- databases.py
+++ databases.py
@@ -10,11 +10,6 @@ mainmenu = Table('mainmenu', metadata,
                  Column('id', Integer(), primary_key=True, autoincrement=True),
                  Column('title', Text(), nullable=False),
                  Column('url', Text(), nullable=False))
-metadata.create_all(engine)
-conn.commit()
-conn.close()
-
-conn = engine.connect()
 posts = Table('posts1', metadata,
               Column('id', Integer(), primary_key=True, autoincrement=True),
               Column('title', Text(), nullable=False),
